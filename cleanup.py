eaed136ae2bb490d33511af4a92985e59a71fdf9
Otto W. Rasmussen <otto.westy.rasmussen@gmail.com>
2024-05-22 00:51:00 +0200

    Add to cleanup

--- cleanup.py
+++ cleanup.py
@@ -1,18 +1,29 @@
-with open("./host/include/tvmgen_default.h", 'r+') as file:
-    count = 0
-    flag = True
-    lines = file.readlines()
-    file.seek(0)
-    for line in lines:
-        if "struct" in line:
-            flag = False
-        if "#define" in line and :
-            flag = True
-        if flag == True:
+def fix_header_file(path):
+    with open(path, 'r+') as file:
+        flag = True
+        lines = file.readlines()
+        file.seek(0)
+        file.truncate()
+        for line in lines:
+            if "struct" in line:
+                flag = False
+            if "#define" in line:
                 flag = True
-                file.write(line)
-            else:
+            if flag == True:
                 file.write(line)
 
-def clean_lib_file(path):
-    return
+def fix_lib_file_0(path):
+    with open(path, 'r+') as file:
+        flag = False
+        lines = file.readlines()
+        file.seek(0)
+        file.truncate()
+        for idx, line in enumerate(lines):
+            if idx != 0 and "int32" not in line and flag == False:
+                file.write(line)
+            if idx == 0:
+                file.write("#include <stdint>")
+            if flag == True:
+                flag = False
+            if "int32" in line:
+                flag = True
