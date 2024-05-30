9e0dabf66209736c8dc8988c73a85f76e1305f6b
Otto W. Rasmussen <otto.westy.rasmussen@gmail.com>
2024-05-31 00:06:43 +0200

    Fixed minor things

--- host/include/tvmgen_default.h
+++ host/include/tvmgen_default.h
@@ -17,6 +17,29 @@ extern "C" {
 /*!
  * \brief Input tensor pointers for TVM module "default" 
  */
+struct tvmgen_default_inputs {
+  void* Input3;
+};
+
+/*!
+ * \brief Output tensor pointers for TVM module "default" 
+ */
+struct tvmgen_default_outputs {
+  void* output;
+};
+
+/*!
+ * \brief entrypoint function for TVM module "default"
+ * \param inputs Input tensors for the module 
+ * \param outputs Output tensors for the module 
+ */
+int32_t tvmgen_default_run(
+  struct tvmgen_default_inputs* inputs,
+  struct tvmgen_default_outputs* outputs
+);
+/*!
+ * \brief Workspace size for TVM module "default" 
+ */
 #define TVMGEN_DEFAULT_WORKSPACE_SIZE 31360
 
 #ifdef __cplusplus
