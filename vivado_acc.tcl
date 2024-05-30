ebcaa6ba08f2267d8f2862de500252ddd4d6016f
Otto W. Rasmussen <otto.westy.rasmussen@gmail.com>
2024-05-31 00:02:43 +0200

    Fixed vivado init script

--- vivado_acc.tcl
+++ vivado_acc.tcl
@@ -1,7 +1,23 @@
-file mkdir /rtl/tmp
-create_project project_acc_project ./rtl/tmp/acc_project
--part xc7a100tcsg324-1
-
-add_files ./rtl/*.vhd
-source /rtl/acc/*.tcl
-add_files ./rtl/acc/*.vhd
+# STEP#1: define the output directory area.
+#
+set outputDir ./synth/
+file mkdir $outputDir
+create_project project_acc ./tmp/acc_project \
+ -part xc7a100tcsg324-1 -force
+#
+# STEP#2: setup design sources and constraints
+#
+add_files [ glob ./rtl/*.vhd]
+foreach f [ glob ./rtl/acc/*.tcl ] {
+   source $f 
+}
+add_files [ glob ./rtl/acc/*.vhd]
+#
+# Physically import the files under project_cpu.srcs/sources_1/imports directory
+import_files -force -norecurse
+#
+# Physically import bft_full.xdc under project_cpu.srcs/constrs_1/imports directory
+import_files -fileset constrs_1 -force -norecurse ./rtl/Nexys4DDR_edge.xdc
+# Update compile order for the fileset 'sources_1'
+set_property top top [current_fileset]
+update_compile_order -fileset sources_1
